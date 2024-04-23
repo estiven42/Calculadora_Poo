743e5229801d6b068dec48d47904e89f5659aa7d
estiven42 <notavop@ucentral.edu.co>
2024-04-23 14:22:21 -0500

    Primer comit

---
+++ src/main/java/co/ucentral/sistemas/Tarjeta_Credito/TarjetaCreditoApplication.java
@@ -0,0 +1,13 @@
+package co.ucentral.sistemas.Tarjeta_Credito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarjetaCreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarjetaCreditoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/ucentral/sistemas/Tarjeta_Credito/TarjetaCreditoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.ucentral.sistemas.Tarjeta_Credito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarjetaCreditoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
